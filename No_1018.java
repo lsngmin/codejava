f01aba42fa23402edcf46e0c8466b5a2f90fc6ad
lsngmin <lsngmin@naver.com>
2021-02-19 01:33:55 +0900

    BOJ

--- No_1018.java
+++ No_1018.java
@@ -1,28 +1,81 @@
 /*
 Problem_1018_체스판 다시 칠하기
 https://www.acmicpc.net/problem/1018
-
-안풀었어
  */
 
-
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class No_1018 {
+    private static void paint() {
+
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int r = 1;
+        String str = "";
+        boolean[][] chess_board;
+        int i, k, count = 0, row, column, max = 9999999;
 
-        if (k > n / 2) { k = n - k; }
+        column = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
 
-        for (int i = n; i > n - k; i--) { r *= i; }
-        for (int i = k; i >= 1; i--) { r /= i; }
-        if (k == n || k == 0) {System.out.println(1);}
-        else { System.out.println(r); }
+        chess_board = new boolean[column][row];
+        /*
+        true is like white.
+        false is black.
+
+        info.
+            66 is ASCII value of 'B'
+            87 is ASCII value of 'W'
+         */
+        for (i = 0; i < chess_board.length; i++) {
+            str = br.readLine();
+            for (k = 0; k < chess_board[i].length; k++) {
+                chess_board[i][k] = str.charAt(k) == 87;
+            }
+        }
+
+        /*
+        if first starting will be black. or
+            if first starting will be white.
+         */
+        for (i = 0; i < column - 7; i++) {// part for column change
+            for (k = 0; k < row - 7; k++) {// part for row change
+                count = 0;
+                boolean first = chess_board[i][k];
+                for (int start = i; start < i + 8; start++) {// test
+                    for (int j = k; j < k + 8; j++) {
+                        if (first != chess_board[start][j]) {
+                            count++;
+                            //chess_board[start][j] = !first;
+                        }
+                        first = !first;
+                    }
+                    first = !first;
+                }
+                if (max > count) {
+                    max = count;
+                }
+                //reverse
+                if (max > 64 - count) {
+                    max = 64 - count;
+                }
+
+            }
+        }
+
+//        for (boolean[] b : chess_board) {
+//            System.out.println(Arrays.toString(b));
+//        }
+
+        br.close();
+        bw.write(max + "");
+        bw.flush();
+        bw.close();
     }
 }
